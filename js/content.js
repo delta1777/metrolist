7037aa4b998507d276373a235ff8a214683a3658
unknown <tuccat68@gmail.com>
2026-04-25 18:31:42 +0300

    1.1.1

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from `./score.js`;
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir =`/metrolist/data`;
+const dir =`metrolist/data`;
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
