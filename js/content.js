a515cf9b55f0d61c8ed91d35af456cad91c055e5
unknown <tuccat68@gmail.com>
2026-04-25 17:59:15 +0300

    1.0.5

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = 'data';
+const dir ='data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
@@ -31,7 +31,7 @@ export async function fetchList() {
             }),
         );
     } catch {
-        console.error(`Failed to load list.`);
+        console.error(`это пиздец.`);
         return null;
     }
 }
