204e4aab94792374a7ef07e381b9219e213f02e1
unknown <tuccat68@gmail.com>
2026-04-25 19:27:21 +0300

    1.2.2

--- js/pages/List.js
+++ js/pages/List.js
@@ -7,11 +7,11 @@ import Spinner from "../components/Spinner.js";
 import LevelAuthors from "../components/List/LevelAuthors.js";
 
 const roleIconMap = {
-    owner: "assets/crown",
-    admin: "assets/user-gear",
-    helper: "assets/user-shield",
-    dev: "assets/code",
-    trial: "assets/user-lock",
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
 };
 
 export default {
